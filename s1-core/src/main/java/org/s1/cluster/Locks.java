579aa740ae27e161b253f7576b92641ab3eed376
Grigory Pykhov <p-gregory@mail.ru>
2014-03-22 15:10:53 +0400

    #30 multi-key, multi-level locks

--- s1-core/src/main/java/org/s1/cluster/Locks.java
+++ s1-core/src/main/java/org/s1/cluster/Locks.java
@@ -16,11 +16,15 @@
 
 package org.s1.cluster;
 
+import com.hazelcast.core.IList;
 import com.hazelcast.core.ILock;
+import com.hazelcast.core.IMap;
 import org.s1.S1SystemError;
 import org.s1.misc.Closure;
 import org.s1.misc.ClosureException;
+import org.s1.objects.Objects;
 
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
@@ -28,6 +32,10 @@ import java.util.concurrent.TimeoutException;
  * Distributed lock helper
  */
 public class Locks {
+
+    private static IMap<String,Long> locks = HazelcastWrapper.getInstance().getMap("s1.locks");
+    private static ILock lock = HazelcastWrapper.getInstance().getLock("s1.locks");
+    private static ThreadLocal<List<String>> localLocks = new ThreadLocal<List<String>>();
 
     /**
      *
@@ -39,7 +47,90 @@ public class Locks {
      * @throws TimeoutException
      */
     public static Object waitAndRun(String lockId, Closure<String, Object> closure, long timeout, TimeUnit tu) throws TimeoutException, ClosureException {
-        ILock lock = HazelcastWrapper.getInstance().getLock(lockId);
+        return waitAndRun(Objects.newArrayList(lockId),closure,timeout,tu);
+    }
+
+    /**
+     *
+     * @param lockIds
+     * @param closure
+     * @param timeout
+     * @param tu
+     * @return
+     * @throws TimeoutException
+     * @throws ClosureException
+     */
+    public static Object waitAndRun(List<String> lockIds, Closure<String, Object> closure, long timeout, TimeUnit tu) throws TimeoutException, ClosureException {
+        if(localLocks.get() == null)
+            localLocks.set(Objects.newArrayList(String.class));
+
+        long t = System.currentTimeMillis();
+        Object ret = null;
+        while(true){
+            boolean b = false;
+            try{
+                /* BEGIN: lock to check */
+                try{
+                    boolean lb = lock.tryLock(timeout,tu);
+                    if(!lb)
+                        throw new TimeoutException("Timeout waiting lock");
+
+                    for(String new_lock:lockIds){
+                        for(String existing_lock:locks.keySet()){
+                            if(new_lock.startsWith(existing_lock) ||
+                                    existing_lock.startsWith(new_lock)){
+                                if(!localLocks.get().contains(existing_lock)){
+                                    b = true;
+                                    break;
+                                }
+                            }
+                        }
+                        if(b)
+                            break;
+                    }
+                    if(!b){
+                        //set locks
+                        for(String new_lock:lockIds){
+                            locks.put(new_lock,System.currentTimeMillis());
+                            localLocks.get().add(new_lock);
+                        }
+                    }
+                } catch (InterruptedException e){
+                    throw S1SystemError.wrap(e);
+                } finally {
+                    try{
+                        lock.unlock();
+                    }catch (Throwable e){}
+                }
+                /* END: lock to check */
+
+                //run code
+                if(!b){
+                    ret = closure.call(null);
+                    break;
+                }
+
+            }finally {
+                //remove locks
+                if(!b){
+                    for(String new_lock:lockIds){
+                        locks.remove(new_lock);
+                        localLocks.get().remove(new_lock);
+                    }
+                }
+            }
+
+            if(System.currentTimeMillis()-t>tu.toMillis(timeout)){
+                throw new TimeoutException("Timeout waiting lock: "+lockIds);
+            }
+            try{
+                Thread.sleep(1);
+            }catch (InterruptedException e){
+                throw S1SystemError.wrap(e);
+            }
+        }
+        return ret;
+        /*ILock lock = HazelcastWrapper.getInstance().getLock("s1.locks");
         boolean b = false;
         try {
             b = lock.tryLock(timeout, tu);
@@ -54,22 +145,7 @@ public class Locks {
             }
         } else {
             throw new TimeoutException("Lock timeout occurs: " + lockId);
-        }
-    }
-
-    /**
-     * Run only if there is not other lock
-     *
-     * @param lockId
-     * @param closure
-     * @return
-     */
-    public static Object runIfFree(String lockId, Closure<String, Object> closure) throws ClosureException {
-        try {
-            return waitAndRun(lockId, closure, 0, TimeUnit.MILLISECONDS);
-        } catch (TimeoutException e) {
-            return null;
-        }
+        }*/
     }
 
 }
--- s1-core/src/test/java/cluster/LockTest.java
+++ s1-core/src/test/java/cluster/LockTest.java
@@ -1,11 +1,16 @@
 package cluster;
 
+import com.hazelcast.core.Hazelcast;
+import org.s1.cluster.HazelcastWrapper;
 import org.s1.cluster.Locks;
 import org.s1.misc.Closure;
 import org.s1.misc.ClosureException;
+import org.s1.objects.Objects;
 import org.s1.test.BasicTest;
 import org.s1.test.LoadTestUtils;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
@@ -44,32 +49,104 @@ public class LockTest extends BasicTest {
         assertEquals(1, buf.length());
     }
 
-    public void testHasLock(){
-        int p = 10;
-        title("Has lock, parallel "+p);
-        final StringBuffer buf = new StringBuffer();
-        final StringBuffer buf2 = new StringBuffer();
+    public void testMultiLock(){
+        HazelcastWrapper.getInstance();
+        int p = 100;
+        title("Multi Lock, parallel "+p);
+        final ConcurrentHashMap<String,Object> cm = new ConcurrentHashMap<String, Object>();
         assertEquals(p, LoadTestUtils.run("test", p, p, new Closure<Integer, Object>() {
             @Override
-            public Object call(Integer index) throws ClosureException {
-
-                Locks.runIfFree("test", new Closure<String, Object>() {
-                    @Override
-                    public Object call(String input) {
-                        buf2.append(".");
-                        if (buf.length() == 0)
-                            buf.append(".");
-                        return null;
-                    }
-                });
+            public Object call(final Integer index) throws ClosureException {
+                try {
+                    String lockId = "db/coll";
+                    if(index%2==0)
+                        lockId = "db/coll/id"+index;
+                    final String lid = lockId;
+                    Locks.waitAndRun(lockId, new Closure<String, Object>() {
+                        @Override
+                        public Object call(String input) {
+                            //no lock with same name
+                            assertFalse(cm.containsValue(lid));
+                            sleep(10);
+                            //put self
+                            cm.put("" + index, lid);
+                            sleep(10);
+                            if(index%2==0){
+                                assertTrue(cm.size() >= 1);
+                            }else{
+                                assertEquals(1,cm.size());
+                            }
+                            sleep(10);
+                            cm.remove(""+index);
+                            return null;
+                        }
+                    }, 30, TimeUnit.SECONDS);
+                } catch (TimeoutException e) {
+                    throw new RuntimeException(e);
+                }
 
                 return null;
             }
         }));
-        trace("were able to run: "+buf2.length());
-        assertEquals(1, buf.length());
-        assertTrue(buf2.length()>=1);
-        assertTrue(buf2.length()<p);
+        assertTrue(cm.size()==0);
+    }
+
+    public void testMultiLockGame(){
+        HazelcastWrapper.getInstance();
+        final int p = 1000;
+        final int c = 20;
+        final int sum = 100;
+        final int pay = 2;
+        title("Multi Lock game, parallel "+p);
+        final Map<String,Integer> cm = Objects.newHashMap();
+        for(int i=0;i<c;i++){
+            cm.put("acc_"+i,sum);
+        }
+        assertEquals(p, LoadTestUtils.run("test", p, p, new Closure<Integer, Object>() {
+            @Override
+            public Object call(final Integer index) throws ClosureException {
+                try {
+                    final int acc1 = (int)Math.floor(Math.random()*10000000%c);
+                    final int acc2 = (int)Math.floor(Math.random() * 10000000 % c);
+                    if(acc1==acc2)
+                        return null;
+
+                    Locks.waitAndRun(Objects.newArrayList(
+                            //"db/coll"
+                            "db/coll/acc_"+acc1,
+                            "db/coll/acc_"+acc2
+                    ), new Closure<String, Object>() {
+                        @Override
+                        public Object call(String input) {
+                            //no lock with same name
+                            int bal1 = cm.get("acc_"+acc1);
+                            int bal2 = cm.get("acc_"+acc2);
+                            if(bal1-pay>=0){
+                                bal1-=pay;
+                                bal2+=pay;
+                            }
+                            sleep(10);
+                            cm.put("acc_"+acc1,bal1);
+                            cm.put("acc_"+acc2,bal2);
+
+                            return null;
+                        }
+                    }, 30, TimeUnit.SECONDS);
+                } catch (TimeoutException e) {
+                    throw new RuntimeException(e);
+                }
+
+                return null;
+            }
+        }));
+
+        //
+        trace(cm);
+        int total = 0;
+        for(int i:cm.values()){
+            total+=i;
+        }
+        assertEquals(sum*c,total);
     }
 
 }
